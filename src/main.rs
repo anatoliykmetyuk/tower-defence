8d59843d44b77b4b1d975933367aace7a6f5e22e
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2023-02-19 21:56:39 +0100

    Initial scene

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use bevy::prelude::*;
+
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_startup_system(spawn_basic_scene)
+        .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Bevy Tower Defence".to_string(),
+                width: WIDTH,
+                height: HEIGHT,
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        })).run();
+}
+
+fn spawn_camera(mut commands : Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.3, 0.3, 0.3).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 1.0),
+        ..default()
+    });
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        point_light: PointLight {
+            intensity: 15000.0,
+            // color: Color::rgb(0.2, 0.5, 0.5),
+            shadows_enabled: true,
+            ..default()
+        },
+        ..default()
+    });
+}
