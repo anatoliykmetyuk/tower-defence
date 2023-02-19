ccbf4c5d1666d0eb42b6a53deb1e1e4cbe32c2f7
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2023-02-19 23:06:54 +0100

    Tower shooting logic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct Tower {
+    shooting_timer: Timer,
+}
+
+#[derive(Resource)]
+pub struct GameAssets {
+    bullet_scene: Handle<Scene>,
+}
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct Lifetime {
+    despawn_timer: Timer,
+}
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
         .add_startup_system(spawn_basic_scene)
         .add_startup_system(spawn_camera)
+        .add_startup_system(asset_loading)
+        .add_system(tower_shooting)
+        .add_system(bullet_despawn)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
                 title: "Bevy Tower Defence".to_string(),
@@ -17,7 +38,11 @@ fn main() {
                 ..default()
             },
             ..default()
-        })).run();
+        }))
+        .add_plugin(WorldInspectorPlugin)
+        .register_type::<Tower>()
+        .register_type::<Lifetime>()
+        .run();
 }
 
 fn spawn_camera(mut commands : Commands) {
@@ -32,13 +57,18 @@ fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
         mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
-    });
+    })
+    .insert(Name::new("Ground"));
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb(0.3, 0.3, 0.3).into()),
         transform: Transform::from_xyz(0.0, 0.5, 1.0),
         ..default()
-    });
+    })
+    .insert(Tower {
+        shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+    })
+    .insert(Name::new("Tower"));
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         point_light: PointLight {
@@ -48,5 +78,50 @@ fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
             ..default()
         },
         ..default()
+    })
+    .insert(Name::new("Light"));
+}
+
+fn tower_shooting(
+    mut commands: Commands,
+    game_assets: ResMut<GameAssets>,
+    mut towers: Query<(&mut Tower, &Transform)>,
+    time: Res<Time>,
+) {
+    for (mut tower, transform) in towers.iter_mut() {
+        if tower.shooting_timer.tick(time.delta()).just_finished() {
+            let spawn_transform = Transform::from_xyz(transform.translation.x, transform.translation.y, transform.translation.z+0.6);
+
+            commands.spawn(SceneBundle {
+                scene: game_assets.bullet_scene.clone(),
+                transform: spawn_transform,
+                ..default()
+            })
+            .insert(Lifetime {
+                despawn_timer: Timer::from_seconds(0.5, TimerMode::Once),
+            })
+            .insert(Name::new("Bullet"));
+        }
+    }
+}
+
+fn bullet_despawn(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut bullets: Query<(Entity, &mut Lifetime)>,
+) {
+    for (entity, mut lifetime) in bullets.iter_mut() {
+        if lifetime.despawn_timer.tick(time.delta()).just_finished() {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn asset_loading(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.insert_resource(GameAssets {
+        bullet_scene: asset_server.load("Tomato.glb#Scene0"),
     });
 }
