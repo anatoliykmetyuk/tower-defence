48c7eea98b122993637635271469aa5c7749b496
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2023-02-20 22:31:46 +0100

    Bullet collisions

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ pub const WIDTH: f32 = 1280.0;
 #[reflect(Component)]
 pub struct Tower {
     shooting_timer: Timer,
+    bullet_offset: Vec3,
 }
 
 #[derive(Resource)]
@@ -57,6 +58,8 @@ fn main() {
         .add_system(bullet_despawn)
         .add_system(move_targets)
         .add_system(move_bullets)
+        .add_system(target_death)
+        .add_system(bullet_collision)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
                 title: "Bevy Tower Defence".to_string(),
@@ -100,6 +103,7 @@ fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
     })
     .insert(Tower {
         shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+        bullet_offset: Vec3::new(0.0, 0.0, 0.6),
     })
     .insert(Name::new("Tower"));
 
@@ -125,28 +129,27 @@ fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
 fn tower_shooting(
     mut commands: Commands,
     game_assets: ResMut<GameAssets>,
-    mut towers: Query<(Entity, &mut Tower, &Transform)>,
-    targets: Query<(&Target, &Transform)>,
+    mut towers: Query<(Entity, &mut Tower, &GlobalTransform)>,
+    targets: Query<&GlobalTransform, With<Target>>,
     time: Res<Time>,
 ) {
     for (entity, mut tower, transform) in towers.iter_mut() {
         if tower.shooting_timer.tick(time.delta()).just_finished() {
-            let spawn_transform = Transform::from_xyz(0.0, 0.0, 0.6);
             // Find the closest target and set the direction vector to point at it
             let direction = targets.iter()
-                .map(|(_, target_transform)| target_transform.translation - spawn_transform.translation)
+                .map(|target_transform| target_transform.translation() - (transform.translation() + tower.bullet_offset))
                 .min_by(|a, b| a.length().partial_cmp(&b.length()).unwrap());
 
             if let Some(direction) = direction {
                 commands.entity(entity).with_children(|commands| {
                     commands.spawn(SceneBundle {
                         scene: game_assets.bullet_scene.clone(),
-                        transform: spawn_transform,
+                        transform: Transform::from_translation(tower.bullet_offset),
                         ..default()
                     })
                     .insert(Bullet {
                         direction: direction.normalize(),
-                        speed: 10.0,
+                        speed: 5.0,
                     })
                     .insert(Lifetime {
                         despawn_timer: Timer::from_seconds(3.0, TimerMode::Once),
@@ -198,7 +201,7 @@ fn spawn_targets(
                 speed: 0.5,
             })
             .insert(Health {
-                value: 10,
+                value: 5,
             })
             .insert(Lifetime {
                 despawn_timer: Timer::from_seconds(10.0, TimerMode::Once),
@@ -223,5 +226,32 @@ fn move_bullets(
 ) {
     for (bullet, mut transform) in bullets.iter_mut() {
         transform.translation += bullet.direction * bullet.speed * time.delta_seconds();
+    }
+}
+
+fn target_death(
+    mut commands: Commands,
+    mut targets: Query<(Entity, &Health)>,
+) {
+    for (entity, health) in targets.iter_mut() {
+        if health.value <= 0 {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn bullet_collision(
+    mut commands: Commands,
+    bullets: Query<(Entity, &GlobalTransform), With<Bullet>>,
+    mut targets: Query<(&mut Health, &GlobalTransform), With<Target>>,
+) {
+    for (bullet_entity, bullet_transform) in &bullets {
+        for (mut health, target_transform) in &mut targets {
+            if Vec3::distance(bullet_transform.translation(), target_transform.translation()) <= 0.25 {
+                health.value -= 1;
+                commands.entity(bullet_entity).despawn_recursive();
+                break;
+            }
+        }
     }
 }
